d3345d59e972ecee003df8018b96d9bb6494123b
NA Flipper <dioncleung@gmail.com>
2020-11-09 00:06:32 -0800

    added functionality

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ submission.addEventListener("click", function (event) {
   event.preventDefault();
   let changeMeStr = inputBox.value;
   let changeMeArr = changeMeStr.split("");
-  let string = "hey";
+  console.log("before: " + changeMeArr);
+  for (let i = 0; i < changeMeArr.length; i++) {
+    let chance = Math.floor(Math.random() * 2);
+    if (chance === 0) {
+      changeMeArr[i] = changeMeArr[i].toLowerCase();
+    } else {
+      changeMeArr[i] = changeMeArr[i].toUpperCase();
+    }
+  }
+  console.log("after: " + changeMeArr);
+  let string = changeMeArr.join("");
+  let paragraph = document.createElement("p");
+  paragraph.textContent = string;
 
-  output.appendChild(string);
+  output.appendChild(paragraph);
 });
