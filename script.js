a2e60016a35d04d72eaba44574d09a1fef7587a8
NA Flipper <dioncleung@gmail.com>
2020-11-09 00:22:36 -0800

    added logic for double L's

--- script.js
+++ script.js
@@ -6,20 +6,32 @@ const output = document.getElementById("output");
 submission.addEventListener("click", function (event) {
   event.preventDefault();
   let changeMeStr = inputBox.value;
-  let changeMeArr = changeMeStr.split("");
-  console.log("before: " + changeMeArr);
-  for (let i = 0; i < changeMeArr.length; i++) {
-    let chance = Math.floor(Math.random() * 2);
-    if (chance === 0) {
-      changeMeArr[i] = changeMeArr[i].toLowerCase();
+  let changeMeArr = changeMeStr.toLowerCase().split("");
+
+  let upperCounter = 0;
+  let lowerCounter = 0;
+  for (let i = 1; i < changeMeArr.length; i++) {
+    if (changeMeArr[i] === "l" && changeMeArr[i + 1] === "l") {
+      changeMeArr[i] = "L";
+      i += 2;
     } else {
-      changeMeArr[i] = changeMeArr[i].toUpperCase();
+      let chance = Math.floor(Math.random() * 2);
+      if (chance === 0 && upperCounter < 3) {
+        changeMeArr[i] = changeMeArr[i].toUpperCase();
+        upperCounter++;
+        lowerCounter = 0;
+      } else if (chance === 0 && lowerCounter < 3) {
+        changeMeArr[i] = changeMeArr[i].toLowerCase();
+        lowerCounter++;
+        upperCounter = 0;
+      }
     }
   }
-  console.log("after: " + changeMeArr);
+
   let string = changeMeArr.join("");
   let paragraph = document.createElement("p");
   paragraph.textContent = string;
 
   output.appendChild(paragraph);
+  inputBox.value = "";
 });
